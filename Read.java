1e3588e2a0e8b516dd650b37197a77474a416456
Kymberly Ayodeji <51680946+kayodeji@users.noreply.github.com>
2020-01-10 07:13:12 -0600

    added the Read.java file

---
+++ Read.java
@@ -0,0 +1,18 @@
+/** This program reads from a file named sample.txt and prints to the screen sentences using the information in the file **/
+//Include below the imported libraries
+
+
+public class Read
+{
+
+    public static void main(String[] args)
+    {
+    
+    
+    
+    
+    
+    }
+
+
+}
